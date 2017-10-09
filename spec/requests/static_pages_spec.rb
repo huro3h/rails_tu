fdb7ffb46d32899fcc4362d84bed20ad00bdc313
huro3h_satoshi <huro3h@gmail.com>
2017-10-09 20:24:02 +0900

    Update files about static_pages tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 describe "Static_pages" do
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "'Sample App'の文言が含まれていること" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample_app')
+    end
+  end
+
+  describe "help page" do
+    it "'helpのページ'の文言が含まれていること" do
+      visit '/static_pages/help'
+      expect(page).to have_content('helpのページ')
+    end
+  end
+
+  describe "About page" do
+    it "'About Us'の文言が含まれていること" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
