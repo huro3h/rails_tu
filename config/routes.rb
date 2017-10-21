39ed2436e9c6186185a2631ef8c3abeb864acc19
huro3h_satoshi <huro3h@gmail.com>
2017-10-21 20:56:11 +0900

    Update files about page links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  # get 'contact', to: 'static_pages#contact', as: '/contact'
   get 'static_pages/contact'
 
   resources :microposts
