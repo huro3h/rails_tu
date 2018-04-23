d72cdbb94819c96efdbdc9b14139a1a655b07ef6
huro3h <huro3h@gmail.com>
2018-04-24 08:34:21 +0900

    Change root page hello to users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users
   # GET /users.json
@@ -62,13 +62,12 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- spec/feature/root_spec.rb
+++ spec/feature/root_spec.rb
@@ -2,9 +2,13 @@ require "rails_helper"
 
 feature "rootページ" do
   context 'rootページにアクセスしたら' do
-    scenario 'Hello~ の文言が表示されていること' do
-      visit "/"
-      expect(page).to have_content "Hello, Rails 5.2"
+    before { visit "/" }
+    scenario 'Hello~ の文言が表示されていないこと' do
+      expect(page).to_not have_content "Hello, Rails 5.2"
+    end
+
+    scenario 'users#indexページへ遷移すること' do
+      expect(page).to have_content "Users"
     end
   end
 end
