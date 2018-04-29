4de63bd4dc3d5cad26284a27cf7f8e6a231d95fd
huro3h <huro3h@gmail.com>
2018-04-29 11:32:50 +0900

    Checked effectiveness of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let!(:user) { User.new(email: 'a@example.com', name: 'test') }
+  it 'Userモデルを正しく作成できること' do
+    expect(user).to be_valid
+  end
 end
