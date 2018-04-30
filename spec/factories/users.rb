1ad5322b17a944c0dc7b638598c266b3a20e5b6c
huro3h <huro3h@gmail.com>
2018-05-01 08:00:31 +0900

    Add test for validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :user do
     name Faker::Name.name
     email Faker::Internet.email
+
+    trait :nil_name do
+      name nil
+    end
+
+    trait :nil_email do
+      email nil
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  let!(:user) { build(:user) }
-  it 'Userモデルを正しく作成できること' do
+  let(:user) { build(:user) }
+  let(:user_no_name) { build(:user, :nil_name) }
+  let(:user_no_email) { build(:user, :nil_email) }
+
+  it '名前とemailに値があれば有効であること' do
     expect(user).to be_valid
+  end
+
+  it '名前は必須項目であること' do
+    expect(user_no_name).to be_invalid
+  end
+
+  it 'emailは必須項目であること' do
+    expect(user_no_email).to be_invalid
   end
 end
