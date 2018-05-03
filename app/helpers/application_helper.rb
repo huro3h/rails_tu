58d105d5fa20564fa1d18102c41d403e49d1092a
huro3h <huro3h@gmail.com>
2018-05-04 02:52:42 +0900

    Defined application_helper#displayed_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def displayed_title(page_title = "")
+    default_title = "rails_tu"
+    if page_title.blank?
+      default_title
+    else
+      "#{page_title} | #{default_title}"
+    end
+  end
 end
