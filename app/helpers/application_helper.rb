cd3ba0de63938cb0250987124ae4665bbc8dfb83
huro3h <huro3h@gmail.com>
2018-05-04 02:56:35 +0900

    More reduce the number of rows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def displayed_title(page_title = "")
     default_title = "rails_tu"
-    if page_title.blank?
-      default_title
-    else
-      "#{page_title} | #{default_title}"
-    end
+    page_title.blank? ? default_title : "#{page_title} | #{default_title}"
   end
 end
