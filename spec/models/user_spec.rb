e0c00565de561e2bae19d216b1ea4800785a05c8
huro3h <huro3h@gmail.com>
2018-05-05 11:15:08 +0900

    Add test due to user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,24 @@ describe User do
   it 'emailは必須項目であること' do
     expect(user_no_email.save).to eq false
   end
+
+  it '50文字以下の名前は有効であること' do
+    user.name = "a" * 50
+    expect(user.save).to eq true
+  end
+
+  it '51文字以上の名前は有効でないこと' do
+    user.name = "a" * 51
+    expect(user.save).to eq false
+  end
+
+  it '255文字以下のemailは有効であること' do
+    user.email = "a" * 243 + "@example.com"
+    expect(user.save).to eq true
+  end
+
+  it '256文字以上のemailは有効でないこと' do
+    user.email = "a" * 244 + "@example.com"
+    expect(user.save).to eq false
+  end
 end
