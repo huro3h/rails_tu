a79b668a7d2cec41911786e486cf9d442f184be7
huro3h <huro3h@gmail.com>
2018-05-06 18:50:14 +0900

    Fix processing timing of flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      flash.now[:success] = 'ログインしました'
+      puts "success!"
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      puts "failed..."
+      render 'new'
+    end
   end
 
   def destroy
--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -10,5 +10,15 @@ feature "loginページ" do
     scenario 'signupページへのリンクが表示されていること' do
       expect(page).to have_link('Sign up now!', href: '/signup')
     end
+
+    scenario 'ログイン失敗の際、ページ遷移によるフラッシュ表示が適切にされていること' do
+      fill_in "Email", with: ""
+      fill_in "Password", with: ""
+      click_button "Log in"
+      expect(page).to have_current_path("/login")
+      expect(page).to have_css(".alert-danger")
+      visit "/"
+      expect(page).to_not have_css(".alert-danger")
+    end
   end
 end
