b7c551aba19a5eaadceda733db2e9b1804a8fe31
huro3h <huro3h@gmail.com>
2018-05-07 08:14:58 +0900

    Add sessions_helper#current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,9 @@
 module SessionsHelper
-  def log_in
+  def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 end
