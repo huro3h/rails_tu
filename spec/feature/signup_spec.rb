a3aa881628b64adb01d228461ffce2ed4cf9156e
huro3h <huro3h@gmail.com>
2018-05-11 01:11:10 +0900

    Fix files for signup test

--- spec/feature/signup_spec.rb
+++ spec/feature/signup_spec.rb
@@ -3,22 +3,22 @@ require "rails_helper"
 feature 'signupページ' do
   context 'signupページにアクセスした時' do
     before { visit "/signup" }
+
     scenario 'ユーザー登録完了後、個別ページへ遷移すること' do
-      fill_in "Name", with: "Taro Maruwa"
-      fill_in "Email", with: "sample@sample.com"
-      fill_in "Password", with: "11111111"
-      fill_in "Confirmation", with: "11111111"
+      fill_in "Name", with: "test_test"
+      fill_in "Email", with: "test@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Confirmation", with: "password"
       click_button 'Create my account'
 
       expect(current_path).to eq "/users/#{User.last.id}"
-      expect(User.last.name).to eq "Taro Maruwa"
-      expect(User.last.email).to eq "sample@sample.com"
+      expect(page).to have_content "Log out"
     end
 
     scenario '必須項目が抜けていたらエラーが表示されること' do
-      fill_in "Email", with: "sample@sample.com"
-      fill_in "Password", with: "11111111"
-      fill_in "Confirmation", with: "11111111"
+      fill_in "Email", with: "test@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Confirmation", with: "password"
       click_button 'Create my account'
 
       expect(page).to have_css '.alert-danger'
